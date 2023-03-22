557be9af20d37d6aeca6624a8a44f6dc5b8ccc03
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-03-22 16:42:50 -0500

    ex1

---
+++ BestBuyPractices.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("Drake's Department");
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach(Department department in departments)
+{
+    Console.WriteLine(department.Name);
+    Console.WriteLine(department.DepartmentID);
+    Console.WriteLine();
+    Console.WriteLine();
+}
